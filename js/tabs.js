02273bdd62014dc9ce4e13c8701dfce7ef2a4de4
Evgeniy9000 <disabled16@mail.ru>
2022-03-21 16:42:11 +0300

    feat:
    add sending data to the server

--- js/tabs.js
+++ js/tabs.js
@@ -1,17 +1,25 @@
-const tabButtons = document.querySelectorAll(".design-list__item ");
-const tabDescription = document.querySelectorAll(".design__descr");
-const tabImages= document.querySelectorAll(".design-images");
-const tabTitles = document.querySelectorAll(".design__title");
+const titleDocument = document.querySelector("title");
+const tabButtons = document.querySelectorAll(".design-list__item");
+const tabDescriptions = document.querySelectorAll(".design__descr");
+const tabImages = document.querySelectorAll(".design-images");
+const tabImage = document.querySelectorAll(".design-block__img");
+const tabTitle = document.querySelectorAll(".design__title");
 
 const changeContent = (array, value) => {
   array.forEach((elem) => {
     if (elem.dataset.tabsField === value) {
-      elem.classList.remove('hidden');
+      elem.classList.remove("hidden");
     } else {
-      elem.classList.add('hidden');
+      elem.classList.add("hidden");
     }
+  });
+};
 
-    
+const changeTitle = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      titleDocument.textContent = elem.textContent;
+    }
   });
 };
 
@@ -19,18 +27,23 @@ tabButtons.forEach((tabButton) => {
   tabButton.addEventListener("click", (event) => {
     const dataValue = tabButton.dataset.tabsHandler;
 
-    changeContent(tabDescription, dataValue);
-    changeContent(tabImages, dataValue);
+
     
+    changeContent(tabDescriptions, dataValue);
+    changeContent(tabImages, dataValue);
+    changeContent(tabImage, dataValue);
+
+    changeContent(tabTitle, dataValue);
+    changeTitle(tabTitle, dataValue);
+
+
+
     tabButtons.forEach((btn) => {
-      if (btn == event.target) {
-        btn.classList.add('design-list__item_active');
+      if (btn === event.target) {
+        btn.classList.add("design-list__item_active");
       } else {
-        btn.classList.remove('design-list__item_active');
+        btn.classList.remove("design-list__item_active");
       }
-
     });
-
   });
-
 });
